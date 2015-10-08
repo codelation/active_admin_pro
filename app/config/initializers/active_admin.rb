1364193f3ef426a2a15c71942303bf51d4f257ec
Brian Pattison <brian@brianpattison.com>
2015-10-08 09:54:48 -0500

    Clean up active admin initializer

--- app/config/initializers/active_admin.rb
+++ app/config/initializers/active_admin.rb
@@ -1,12 +1,13 @@
 # Include Font Awesome in ActiveAdmin
-ActiveAdmin.application.register_stylesheet "//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css"
+config = ActiveAdmin.application
+config.register_stylesheet "//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css"
 
 # Add Apple web application icons
 base_url = "//s3.amazonaws.com/codelation.activeadmin/apple-touch-icon"
 sizes = [76, 120, 152, 167, 180]
-ActiveAdmin.application.register_stylesheet "#{base_url}.png", media: nil, rel: "apple-touch-icon"
+config.register_stylesheet "#{base_url}.png", media: nil, rel: "apple-touch-icon"
 sizes.each do |size|
-  ActiveAdmin.application.register_stylesheet(
+  config.register_stylesheet(
     "#{base_url}-#{size}x#{size}.png",
     media: nil,
     rel:   "apple-touch-icon",
@@ -15,7 +16,7 @@ sizes.each do |size|
 end
 
 # Add the meta tags to enable Active Admin as an iOS home screen app
-ActiveAdmin.application.meta_tags ||= {}
-ActiveAdmin.application.meta_tags["apple-mobile-web-app-capable"] ||= "yes"
-ActiveAdmin.application.meta_tags["apple-mobile-web-app-title"]   ||= "Admin"
-ActiveAdmin.application.meta_tags["viewport"]                     ||= "width=device-width, initial-scale=1.0"
+config.meta_tags ||= {}
+config.meta_tags["apple-mobile-web-app-capable"] ||= "yes"
+config.meta_tags["apple-mobile-web-app-title"]   ||= "Admin"
+config.meta_tags["viewport"]                     ||= "width=device-width, initial-scale=1.0"
