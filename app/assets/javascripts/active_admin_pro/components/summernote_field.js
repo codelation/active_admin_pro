4083c07c86366f3eda3626d4eee3435b609d7750
Brian Pattison <brian@brianpattison.com>
2015-10-13 13:20:45 -0500

    Add JS events for summernote field focus

--- app/assets/javascripts/active_admin_pro/components/summernote_field.js
+++ app/assets/javascripts/active_admin_pro/components/summernote_field.js
@@ -4,5 +4,35 @@
 // Summer note editor field.
 // @see http://summernote.org
 App.ready(function() {
-  $('.input.summernote textarea').summernote();
+  "use strict";
+
+  var toolbar = [
+    ['style', ['bold', 'italic', 'underline', 'clear']],
+    ['fontsize', ['fontsize']],
+    ['color', ['color']],
+    ['para', ['ul', 'ol', 'paragraph']],
+    ['table', ['table']],
+    ['insert', ['link', 'picture', 'video']],
+    ['code', ['codeview', 'fullscreen']]
+  ];
+
+  $('.input.summernote').each(function() {
+    var wrapper = $(this);
+    var summernoteField = wrapper.find('textarea').summernote({
+      height:  250,
+      toolbar: toolbar,
+
+      onBlur: function(e) {
+        wrapper.removeClass('focused');
+      },
+
+      onFocus: function(e) {
+        wrapper.addClass('focused');
+      }
+    });
+
+    wrapper.find('label').click(function() {
+      summernoteField.summernote('focus');
+    });
+  });
 });
