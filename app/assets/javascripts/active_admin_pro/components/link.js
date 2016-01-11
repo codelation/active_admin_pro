a622c96fed47b299a8e72c065097debafcaac1b9
Brian Pattison <brian@brianpattison.com>
2016-01-11 12:55:02 -0600

    Fix issue with batch destroy functionality being blocked by loading overlay

--- app/assets/javascripts/active_admin_pro/components/link.js
+++ app/assets/javascripts/active_admin_pro/components/link.js
@@ -1,7 +1,7 @@
 App.ready(function() {
   "use strict";
   var body = $('body');
-  var links = $('a:not([data-method="delete"]):not(.has_many_add):not(.dropdown_menu_button):not([target="_blank"])');
+  var links = $('a:not([data-method="delete"]):not([data-action="destroy"]):not(.has_many_add):not(.dropdown_menu_button):not([target="_blank"])');
 
   // Add active class on click to style while loading via turbolinks
   // and add loading class to the body element.
