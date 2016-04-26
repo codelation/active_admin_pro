5a478c0c5b8b70244e5376e7fab92029c65d668c
Brian Pattison <brian@brianpattison.com>
2016-04-26 10:28:46 -0500

    Remove overlay on page loading

--- app/assets/javascripts/active_admin_pro/components/link.js
+++ app/assets/javascripts/active_admin_pro/components/link.js
@@ -3,18 +3,19 @@
 
   var body, links;
 
-  // Add active class on click to style while loading via turbolinks
-  // and add loading class to the body element.
   App.register('component').enter(function() {
     body = $('body');
     links = $('a:not([data-method="delete"]):not([data-action="destroy"]):not(.has_many_add):not(.dropdown_menu_button):not([target="_blank"])');
 
+    // Add 'loading' class to the body when a link is clicked,
+    // add add 'active' class to the clicked link.
     links.click(function() {
+      links.removeClass('active');
       body.addClass('loading');
       $(this).addClass('active');
     });
   }).exit(function() {
-    // Remove loading and active classes on page unload
+    // Remove the classes when the body is unloaded
     body.removeClass('loading');
     links.removeClass('active');
   });
