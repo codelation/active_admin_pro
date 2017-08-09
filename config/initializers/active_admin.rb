a72068dae1ee8d32a8bc8c684f5d88ab2644c269
Jaryd Krishnan <jaryd@harvestprofit.com>
2017-08-09 17:43:52 -0500

    Remove references to "register_stylesheets"

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -8,22 +8,6 @@ ActiveAdmin.application.tap do |config|
   # Prefer short date and time format
   config.localize_format = :short
 
-  # Include Font Awesome in ActiveAdmin
-  config.register_stylesheet "//maxcdn.bootstrapcdn.com/font-awesome/#{ENV['FONT_AWESOME_VERSION'] || '4.5.0'}/css/font-awesome.min.css"
-
-  # Add Apple web application icons
-  base_url = "//s3.amazonaws.com/codelation.activeadmin/apple-touch-icon"
-  sizes = [76, 120, 152, 167, 180]
-  config.register_stylesheet "#{base_url}.png", media: nil, rel: "apple-touch-icon"
-  sizes.each do |size|
-    config.register_stylesheet(
-      "#{base_url}-#{size}x#{size}.png",
-      media: nil,
-      rel:   "apple-touch-icon",
-      sizes: "#{size}x#{size}"
-    )
-  end
-
   # Add the meta tags to enable Active Admin as an iOS home screen app
   config.meta_tags ||= {}
   config.meta_tags["apple-mobile-web-app-capable"] ||= "yes"
